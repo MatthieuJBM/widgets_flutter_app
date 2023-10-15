d283cf224bf095a321b1dcaf912f3a81143cbbc5
Mateusz Baryla <m.baryla@protonmail.ch>
2023-10-15 13:49:11 +0200

    home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,57 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({
+    // No necesitamos key porque es privado y ésa es la única referencia
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    // appMenuItems;
+    // appMenuItems[0].title;
+
+    // Si hay palabra builder, eso quiere decir que se va
+    // a construir en tiempo de la ejecución.
+    return ListView.builder(
+      //physics: BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        //return Text(menuItem.title);
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      // leading es algo que podemos colocar antes de title
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        // TODO: navegar a otra pantalla
+      },
     );
   }
 }
